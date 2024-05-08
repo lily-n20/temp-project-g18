5d0c1302bc92f76af7b8c06bb0c7b554559a6c27
Lily Nguyen <lilynguyen@Lilys-Laptop.local>
2024-05-08 13:12:10 -0400

    deploy again?

---
+++ doc/src/components/sections/Portfolio.js
@@ -0,0 +1,194 @@
+import { salimovSlider } from "@/src/sliderProps";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const Portfolio = () => {
+  return (
+    <section
+      className="portfolio main-section flex-column-mobile"
+      id="portfolio"
+    >
+      {/* TITLE STARTS */}
+      <div className="custom-title">
+        {/* MAIN TITLE STARTS */}
+        <h3>
+          <span>
+            <span className="animated-layer fade-in-left-animation fadeInUp wow">
+              App Overview
+            </span>
+          </span>
+        </h3>
+        {/* MAIN TITLE ENDS */}
+      </div>
+      {/* TITLE ENDS */}
+      <Swiper
+        {...salimovSlider.portfolio}
+        className="swiper swiper-portfolio animated-layer fade-in-right-animation fadeInUp wow"
+        data-wow-offset={200}
+      >
+        {/* PORTFOLIO ITEM STARTS */}
+        <SwiperSlide className="single-item swiper-slide">
+          {/* ITEM MAIN CONTENT STARTS */}
+          <div className="main-content">
+            <img
+              className="img-fluid"
+              src="assets/GooglePlay-Logo.png"
+              alt="Image Project"
+            />
+          </div>
+          {/* ITEM MAIN CONTENT ENDS */}
+          {/* ITEM DETAILS STARTS */}
+          <div className="details">
+            <h4>Get the App!</h4>
+            <div>
+              <ul>
+                <li>
+                  <span>
+                     Where :
+                  </span>
+                  <span>Google Playstore</span>
+                </li>
+                <li>
+                  <span>
+                     Cost :
+                  </span>
+                  <span>Free</span>
+                </li>
+              </ul>
+            </div>
+            <a href="#" target="_blank" className="custom-btn">
+              <span>
+                preview <i className="fa-solid fa-arrow-up-right-from-square" />
+              </span>
+            </a>
+          </div>
+          {/* ITEM DETAILS ENDS */}
+        </SwiperSlide>
+        {/* PORTFOLIO ITEM ENDS */}
+        {/* PORTFOLIO ITEM STARTS */}
+        <SwiperSlide className="single-item swiper-slide">
+          {/* ITEM MAIN CONTENT STARTS */}
+          <div className="main-content">
+            <div className="videocontainer">
+              <iframe
+                className="youtube-video"
+                src="https://www.youtube.com/embed/NCqjRyK2wtA"
+                allowFullScreen=""
+              />
+            </div>
+          </div>
+          {/* ITEM MAIN CONTENT ENDS */}
+          {/* ITEM DETAILS STARTS */}
+          <div className="details">
+            <h4>Demo Video</h4>
+            <div>
+              <ul>
+                <li>
+                  <span>
+                    <i className="fa-regular fa-file-lines" /> Project :
+                  </span>
+                  <span>LingoTune</span>
+                </li>
+                <li>
+                  <span>
+                    <i className="fa-regular fa-user" /> Group :
+                  </span>
+                  <span>18</span>
+                </li>
+                <li>
+                  <span>
+                    <i className="fa-regular fa-hourglass" /> Duration :
+                  </span>
+                  <span>1 min</span>
+                </li>
+                <li>
+                  <span>
+                    <i className="fa-solid fa-code-branch" /> Frameworks :
+                  </span>
+                  <span>React Native, Thunkable, Firebase</span>
+                </li>
+              </ul>
+            </div>
+            <a href="#" target="_blank" className="custom-btn">
+              <span>
+                preview <i className="fa-solid fa-arrow-up-right-from-square" />
+              </span>
+            </a>
+          </div>
+          {/* ITEM DETAILS ENDS */}
+        </SwiperSlide>
+        {/* PORTFOLIO ITEM ENDS */}
+        {/* PORTFOLIO ITEM STARTS */}
+        <SwiperSlide className="single-item swiper-slide">
+          {/* ITEM MAIN CONTENT STARTS */}
+          <div className="main-content">
+            <Swiper
+              {...salimovSlider.portfolioItems}
+              className="swiper swiper-portfolio-item"
+            >
+              <SwiperSlide className="swiper-slide">
+                <img src="assets/screens/Lingo-Register.png" title="img" style = {{
+                  width: '220px',
+                  height: '400px',
+                  marginLeft: '220px',
+                  marginRight: '30px',
+                  borderRadius: '20px'
+                }} />
+              </SwiperSlide>
+              <SwiperSlide className="swiper-slide">
+                <img src="assets/screens/Lingo-Overview.png" title="img" style = {{
+                  width: '220px',
+                  height: '400px',
+                  marginLeft: '220px',
+                  marginRight: '30px',
+                  borderRadius: '20px'
+                }}/>
+              </SwiperSlide>
+              <SwiperSlide className="swiper-slide">
+                <img src="assets/screens/Lingo-Question.png" title="img" style = {{
+                  width: '220px',
+                  height: '400px',
+                  marginLeft: '220px',
+                  marginRight: '30px',
+                  borderRadius: '20px'
+                }}/>
+              </SwiperSlide>
+              <div className="swiper-pagination" />
+            </Swiper>
+          </div>
+          {/* ITEM MAIN CONTENT ENDS */}
+          {/* ITEM DETAILS STARTS */}
+          <div className="details">
+            <h4>How It Looks</h4>
+            <div>
+              <ul>
+                <li>
+                  <span>Mobile and Tablet View</span>
+                </li>
+              </ul>
+            </div>
+            <a href="#" target="_blank" className="custom-btn">
+              <span>
+                preview <i className="fa-solid fa-arrow-up-right-from-square" />
+              </span>
+            </a>
+          </div>
+          {/* ITEM DETAILS ENDS */}
+        </SwiperSlide>
+        {/* PORTFOLIO ITEM ENDS */}
+        {/* PORTFOLIO ITEM STARTS */}
+        <div className="nav-item next-item animated-btn">
+          <span />
+        </div>
+        <div className="nav-item prev-item animated-btn">
+          <span />
+        </div>
+      </Swiper>
+      <img
+        alt=""
+        className="separator hide-mobile"
+        src="assets/separator.png"
+      />
+    </section>
+  );
+};
+export default Portfolio;
