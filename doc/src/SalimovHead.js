de52e59c460a4975c2ad7adefee06f86279402dc
Lily Nguyen <lilynguyen@Lilys-Laptop.local>
2024-05-08 13:58:47 -0400

    please deploy

--- doc/src/SalimovHead.js
+++ doc/src/SalimovHead.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 const SalimovHead = () => {
   return (
     <Head>
-      <title>Salimov - Horizontal Personal Portfolio</title>
+      <title>LingoTune</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Template Google Fonts */}
